b16467025e40cb2b1300000f73f9bffd0684b8ad
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-22 00:30:41 +0300

    Mission Passed

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -34,10 +34,15 @@ def remove_element_from_end_of_list(list):
     list.pop(3)
     print(list)
     return(list)
-remove_element_from_end_of_list([1, 2, 3, 4, 5])
+remove_element_from_end_of_list([1, 2, 3, 4])
 
-def remove_element_from_start_of_list(l):
-    return None
+def remove_element_from_start_of_list(list):
+    list = [1, 2, 3, 4]
+    list.pop(0)
+    print(list)
+    return(list)
+
+remove_element_from_end_of_list(  list = [1, 2, 3, 4])
 
 def retrieve_first_element_from_list(l):
     return None
